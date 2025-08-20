6b2ed29565583c04466bdf2460027a11094e17c4
Vaibhav Dongre <21vaibhavdongre@gmail.com>
2025-08-20 17:57:27 +0530

    Inital setup done

---
+++ main.py
@@ -0,0 +1,39 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def processCommand(c):
+    print(c)  
+
+if __name__ == "__main__":
+    speak("Initializing Jarvis.....")   
+    while True:
+        #Listen for the wake word "Jarvis"
+        # Obtaian audio from the mic
+        r = sr.Recognizer()
+        
+        print("Recognizing...")
+        try:
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source, timeout=2, phrase_time_limit=1) #for faster execution
+            word = r.recognize_google(audio)
+            if(word.lower() == "jarvis"):
+                speak("Yes Sir")
+                #Listen for command
+                with sr.Microphone() as source:
+                    print("Jarvis Active...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+                    processCommand(command)
+
+        except sr.RequestError as e:
+            print("Error; {0}".format(e))              
