d807ec5791c581dfbd4003e7a0495f38c2d64d1b
Vaibhav Dongre <21vaibhavdongre@gmail.com>
2025-08-22 16:33:41 +0530

    Music feature added

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "Blue Eyes" : "https://www.youtube.com/watch?v=NbyHNASFi6U&list=RDEMulALs7qwkePpaGzHG_ttvg&start_radio=1&ab_channel=T-Series",
+
+    "Brown Rang" : "https://www.youtube.com/watch?v=eizmCZv3aKI&list=RDEMulALs7qwkePpaGzHG_ttvg&index=2&ab_channel=SpeedRecords",
+
+    "Desi Kalakar" : "https://www.youtube.com/watch?v=jTg15DrXhB0&list=RDEMulALs7qwkePpaGzHG_ttvg&index=3&pp=8AUB"
+}
